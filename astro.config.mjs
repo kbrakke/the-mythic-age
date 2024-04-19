82427a634780c55ce041ebdc033ff69b8d3570d1
Kris Brakke <kbrakke@gmail.com>
2024-04-19 10:02:31 -0400

    implement content schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-4--the-mythic-age.netlify.app/"
+});
