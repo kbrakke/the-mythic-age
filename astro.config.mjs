2282f772a4c7fb569487fb7fb1750d64ebc8140d
Kris Brakke <kbrakke@gmail.com>
2024-04-23 08:42:52 -0400

    Add stubs for campagins

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,32 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'The Mythic Age',
-    social: {
-      github: 'https://github.com/kbrakke/the-mythic-age'
-    },
-    sidebar: [{
-      label: 'Character Options',
-      autogenerate: {
-        directory: 'resources'
-      }
-    }, {
-      label: 'Weapons',
-      autogenerate: {
-        directory: 'weapons'
-      }
-    }, {
-      label: 'World',
-      autogenerate: {
-        directory: 'world'
-      }
-    }]
-  }), react()]
+	integrations: [starlight({
+		title: 'The Mythic Age',
+		social: {
+			github: 'https://github.com/kbrakke/the-mythic-age'
+		},
+		sidebar: [
+			{
+				label: 'Campaigns',
+				autogenerate: {
+					directory: 'campaigns'
+				}
+			}, {
+				label: 'Character Options',
+				autogenerate: {
+					directory: 'resources'
+				}
+			}, {
+				label: 'Weapons',
+				autogenerate: {
+					directory: 'weapons'
+				}
+			}, {
+				label: 'World',
+				autogenerate: {
+					directory: 'world'
+				}
+			}]
+	}), react()]
 });
